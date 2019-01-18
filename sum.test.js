5b8a730e6bf74ca8006978447093d0b0bbf1b97d
Inuasuraz <kiatsiri_4046@hotmail.co.th>
2019-01-18 15:46:25 +0700

    Second Commit

--- sum.test.js
+++ sum.test.js
@@ -15,14 +15,18 @@ test('2 + 5 เท่ากับ 7 ', function(){
 })
 
 // ทำอีก 3 อัน โดยการพิมพ์เอง อย่า copy
+
+// 5+7 = 12
 test('5 + 7 เท่ากับ 12 ' , function(){
   expect(sum(5,7)).toBe(12);
 })
 
+// 100+200 = 300
 test('100 + 200 เท่ากับ 300' , function() {
   expect(sum(100,200)).toBe(300);
 })
 
+// 5+5 = 10
 test('5 + 5 เท่ากับ 10' , function(){
   expect(sum(5,5)).toBe(10);
 })
